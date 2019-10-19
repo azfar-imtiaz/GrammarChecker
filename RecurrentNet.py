83cd6e919e352651df5385f1e10e8b8e760040a4
azfar-imtiaz <azfar.imtiaz@live.com>
2019-10-20 01:54:11 +0200

    Add dropout

--- RecurrentNet.py
+++ RecurrentNet.py
@@ -3,19 +3,19 @@ import torch.nn as nn
 
 
 class RecurrentNet(nn.Module):
-    def __init__(self, seq_len, vocab_size, embedding_size, num_layers, hidden_size, output_size):
+    def __init__(self, seq_len, vocab_size, embedding_size, num_layers, hidden_size, output_size, dropout):
         super().__init__()
         self.num_layers = num_layers
         self.hidden_size = hidden_size
         self.emb = nn.Embedding(vocab_size, embedding_size)
         self.lstm1 = nn.LSTM(embedding_size, hidden_size,
-                             num_layers=self.num_layers, batch_first=True)
+                             num_layers=self.num_layers, batch_first=True, dropout=dropout)
         # self.lstm2 = nn.LSTM(hidden_size, hidden_size,
         #                      num_layers=self.num_layers)
         # self.lstm3 = nn.LSTM(hidden_size, hidden_size,
         #                      num_layers=self.num_layers)
         self.fc = nn.Linear(hidden_size * seq_len, output_size)
-        self.sigmoid = nn.Sigmoid()
+        # self.sigmoid = nn.Sigmoid()
 
     def forward(self, sequence, hidden_layer):
         output = self.emb(sequence)
@@ -26,7 +26,7 @@ class RecurrentNet(nn.Module):
         output = output.contiguous().view(-1, self.hidden_size *
                                           len(sequence[0]))
         output = self.fc(output)
-        output = self.sigmoid(output)
+        # output = self.sigmoid(output)
         return output, hidden_layer
 
     def init_hidden(self, seq_len):
