a13669a14f3bd4d871c33c7fb227137dc8b585fe
azfar-imtiaz <azfar.imtiaz@live.com>
2019-10-20 01:32:49 +0200

    Resolve batch_first confusion, use torch.nn.F.binary_cross_entropy instead of BCELoss

--- RecurrentNet.py
+++ RecurrentNet.py
@@ -9,11 +9,11 @@ class RecurrentNet(nn.Module):
         self.hidden_size = hidden_size
         self.emb = nn.Embedding(vocab_size, embedding_size)
         self.lstm1 = nn.LSTM(embedding_size, hidden_size,
-                             num_layers=self.num_layers)
-        self.lstm2 = nn.LSTM(hidden_size, hidden_size,
-                             num_layers=self.num_layers)
-        self.lstm3 = nn.LSTM(hidden_size, hidden_size,
-                             num_layers=self.num_layers)
+                             num_layers=self.num_layers, batch_first=True)
+        # self.lstm2 = nn.LSTM(hidden_size, hidden_size,
+        #                      num_layers=self.num_layers)
+        # self.lstm3 = nn.LSTM(hidden_size, hidden_size,
+        #                      num_layers=self.num_layers)
         self.fc = nn.Linear(hidden_size * seq_len, output_size)
         self.sigmoid = nn.Sigmoid()
 
@@ -21,12 +21,12 @@ class RecurrentNet(nn.Module):
         output = self.emb(sequence)
         # hidden_layer = self.init_hidden(len(sequence[0]))
         output, hidden_layer = self.lstm1(output, hidden_layer)
-        output, hidden_layer = self.lstm2(output, hidden_layer)
+        # output, hidden_layer = self.lstm2(output, hidden_layer)
         # output, hidden_layer = self.lstm3(output, hidden_layer)
         output = output.contiguous().view(-1, self.hidden_size *
                                           len(sequence[0]))
         output = self.fc(output)
-        # output = self.sigmoid(output)
+        output = self.sigmoid(output)
         return output, hidden_layer
 
     def init_hidden(self, seq_len):
--- train_recurrent_net.py
+++ train_recurrent_net.py
@@ -2,7 +2,8 @@ import torch
 from torch.utils import data
 from torch.optim import Adam
 # from torch.nn import CrossEntropyLoss
-from torch.nn import BCELoss
+# from torch.nn import BCELoss
+from torch.nn.functional import binary_cross_entropy
 from torch.nn.utils.rnn import pad_sequence
 
 import config
@@ -54,16 +55,19 @@ if __name__ == '__main__':
     print("Padding sequences...")
     sents_padded_train = pad_sequence(
         [torch.LongTensor(seq) for seq in sents_numeric_train], batch_first=True)
+
     # Add a random list of size equal to length of longest sequence in training set, so that length of
     # padded sequences for dev set is same. We will remove this later
     # This needs to be done as we can't specify length in PyTorch's pad_sequence. Mauybe look into Keras
     # functionality of this?
-    sents_numeric_dev.append([12] * sents_padded_train.size(1))
+    sents_numeric_dev.append([0] * sents_padded_train.size(1))
     sents_padded_dev = pad_sequence(
         [torch.LongTensor(seq) for seq in sents_numeric_dev], batch_first=True)
+    # remove this dummy list added to the end of the dev set
     sents_padded_dev = sents_padded_dev[:-1]
 
     # sents_padded_train = sents_padded_train[:2000]
+    # labels_train = labels_train[:2000]
     print("Total training examples: {}".format(len(sents_padded_train)))
 
     print("Creating data generator...")
@@ -71,8 +75,7 @@ if __name__ == '__main__':
         'batch_size': config.BATCH_SIZE,
         'shuffle': True
     }
-    training_set = Dataset(sents_padded_train, [
-                           torch.Tensor(l) for l in labels_train])
+    training_set = Dataset(sents_padded_train, labels_train)
     training_generator = data.DataLoader(training_set, **params)
 
     # dev_set = Dataset(sents_padded_dev, labels_dev)
@@ -84,13 +87,18 @@ if __name__ == '__main__':
     model = model.to(device)
     optimizer = Adam(model.parameters(), lr=config.LEARNING_RATE)
     # criterion = CrossEntropyLoss()
-    criterion = BCELoss()
+    # criterion = BCELoss()
+    criterion = binary_cross_entropy
 
     for epoch in range(config.NUM_EPOCHS):
         print("Epoch number: {}".format(epoch + 1))
         epoch_loss = 0.0
-        hidden_layer = model.init_hidden(sents_padded_train.size(1))
+        # hidden_layer = model.init_hidden(sents_padded_train.size(1))
+        hidden_layer = model.init_hidden(config.BATCH_SIZE)
         for i, (local_batch, local_labels) in enumerate(training_generator):
+            # skip the batch if it contains less elements than batch size
+            if local_batch.size(0) != config.BATCH_SIZE:
+                continue
             optimizer.zero_grad()
             # hidden_layer = hidden_layer.data
             hidden_layer = tuple([e.data for e in hidden_layer])
@@ -100,8 +108,12 @@ if __name__ == '__main__':
             hidden_layer = tuple(hidden_layer)
             local_batch = local_batch.to(device)
             local_labels = local_labels.to(device)
+
+            # binary cross entropy requires size of [batch_size, 1] for labels
+            local_labels = local_labels.view(-1, 1)
+
             output, hidden_layer = model(local_batch, hidden_layer)
-            loss = criterion(output, local_labels)
+            loss = criterion(output, local_labels.float())
             loss.backward()
             epoch_loss += loss.item()
             optimizer.step()
@@ -112,7 +124,8 @@ if __name__ == '__main__':
     total_predictions, correct_predictions = 0, 0
     for i, (padded_sent, label) in enumerate(zip(sents_padded_dev, labels_dev)):
         total_predictions += 1
-        hidden_layer = model.init_hidden(sents_padded_dev.size(1))
+        # hidden_layer = model.init_hidden(sents_padded_dev.size(1))
+        hidden_layer = model.init_hidden(config.BATCH_SIZE)
         hidden_layer = list(hidden_layer)
         hidden_layer[0] = hidden_layer[0].to(device)
         hidden_layer[1] = hidden_layer[1].to(device)
