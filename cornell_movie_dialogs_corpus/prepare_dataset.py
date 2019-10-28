4c8865d2f886ab95fb4efe56ee60aee5af05112f
azfar-imtiaz <azfar.imtiaz@live.com>
2019-10-28 23:45:31 +0100

    Add pluralize/singularize inversion errors and corrections into dataset

--- cornell_movie_dialogs_corpus/prepare_dataset.py
+++ cornell_movie_dialogs_corpus/prepare_dataset.py
@@ -1,7 +1,9 @@
 import re
+import spacy
 import random
 import joblib
-from nltk.tokenize import sent_tokenize
+import inflection
+# from nltk.tokenize import sent_tokenize
 
 
 def read_data_from_file(filename):
@@ -34,24 +36,60 @@ def remove_component_from_sent(rg, sent):
     return None
 
 
-def create_seq_mapping(lines, amount_artical_removal, amount_verb_cont_removal, amount_correct):
+def invert_singular_plural_noun(doc, pos_tags, sent):
+    nouns_count = pos_tags.count("NN") + pos_tags.count("NNS")
+    rand_noun_index = random.randint(0, nouns_count - 1)
+    current_noun_count = 0
+    word_to_change = None
+    is_plural = False
+    for index, word in enumerate(doc):
+        # text = word.text
+        if word.tag_ == 'NN' or word.tag_ == 'NNS':
+            if current_noun_count == rand_noun_index:
+                word_to_change = word.text
+                if word.tag_ == 'NNS':
+                    is_plural = True
+                break
+            else:
+                current_noun_count += 1
+
+    inverted_word = None
+    m = re.search(r'\b%s\b' % word_to_change, sent)
+    if m:
+        if is_plural:
+            inverted_word = inflection.singularize(word_to_change)
+        else:
+            inverted_word = inflection.pluralize(word_to_change)
+        alt_s = re.sub(r'(?<=\b)%s(?=\b)' % word_to_change, inverted_word, sent, count=1)
+        return alt_s
+
+    return None
+
+
+def create_seq_mapping(lines, amount_artical_removal, amount_verb_cont_removal, amount_singular_plural, amount_correct, nlp=None):
     rg_article = r'\b(a|an|the)\b'
     # rg_verb_contraction = r'(?<=\w)\'[a-z]{1,2}\b'
     rg_verb_contraction = r'(?<=\w)\'[(ve)(s)(d)(m)(re)(ll)]{1,2}(?!\')\b'
     random.shuffle(lines)
+
     altered_lines_incorrect = []
     altered_lines_correct = []
-    amount_incorrect = amount_artical_removal + amount_verb_cont_removal
+    amount_incorrect = amount_artical_removal + amount_verb_cont_removal + amount_singular_plural
     article_removed_count = 0
     verb_cont_removed_count = 0
+    singular_plural_inverted_count = 0
     for index, s in enumerate(lines):
         if index % 5000 == 0 and index > 0:
             print("\t{} rows processed!".format(index))
+        doc = nlp(s)
+        pos_tags = [w.tag_ for w in doc]
 
-        # first check if sentence contains either an article or verb contraction or both - if not, skip it
-        art_found = re.search(rg_article, s)
-        vb_found = re.search(rg_verb_contraction, s)
-        if art_found is None and vb_found is None:
+        # check if sentence contains article or verb contraction or singular/plural noun - if not, skip it
+        art_found = bool(re.search(rg_article, s))
+        vb_found = bool(re.search(rg_verb_contraction, s))
+        pos_tag_found = True if 'NN' in pos_tags or 'NNS' in pos_tags else False
+
+        if art_found is None and vb_found is None and pos_tag_found is False:
             continue
 
         # if both article and verb cont found, and we have not reached the limit for both article removal and
@@ -78,6 +116,11 @@ def create_seq_mapping(lines, amount_artical_removal, amount_verb_cont_removal, 
             verb_cont_removed_count += 1
             altered_lines_incorrect.append((alt_s, s))
 
+        elif pos_tag_found and singular_plural_inverted_count < amount_singular_plural:
+            alt_s = invert_singular_plural_noun(doc, pos_tags, s)
+            singular_plural_inverted_count += 1
+            altered_lines_incorrect.append((alt_s, s))
+
         # if either article or verb contraction found and we haven't reached the limit for correct sents
         elif len(altered_lines_correct) < amount_correct:
             altered_lines_correct.append((s, s))
@@ -86,6 +129,10 @@ def create_seq_mapping(lines, amount_artical_removal, amount_verb_cont_removal, 
         if len(altered_lines_correct) >= amount_correct and len(altered_lines_incorrect) >= amount_incorrect:
             break
 
+    print("\nSentences with articles altered: {}".format(article_removed_count))
+    print("Sentences with verb contractions altered: {}".format(verb_cont_removed_count))
+    print("Sentences with singular/plural nouns inverted: {}".format(singular_plural_inverted_count))
+    print("Sentences left unaltered: {}\n".format(len(altered_lines_correct)))
     return altered_lines_correct, altered_lines_incorrect
 
 
@@ -94,12 +141,17 @@ if __name__ == '__main__':
     print("Reading data from file...")
     sents = read_data_from_file(filename)
 
+    print("Loading Spacy English model...")
+    nlp = spacy.load('en_core_web_sm')
+
     print("Creating sequence mapping...")
+    # sents = sents[:1000]
     amount_artical_removal = int(0.15 * len(sents))
     amount_verb_cont_removal = int(0.15 * len(sents))
+    amount_sing_plural_invertion = int(0.15 * len(sents))
     amount_correct = int(0.1 * len(sents))
 
-    correct_sents, incorrect_sents = create_seq_mapping(sents, amount_artical_removal, amount_verb_cont_removal, amount_correct)
+    correct_sents, incorrect_sents = create_seq_mapping(sents, amount_artical_removal, amount_verb_cont_removal, amount_sing_plural_invertion, amount_correct, nlp)
     dataset = incorrect_sents + correct_sents
 
     print("Shuffling dataset...")
