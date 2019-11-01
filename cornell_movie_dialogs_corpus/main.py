76ce4c383bc1af9f12979521bc6b1e9c325a32a6
azfar-imtiaz <azfar.imtiaz@live.com>
2019-11-01 20:19:06 +0100

    Use full dataset

--- cornell_movie_dialogs_corpus/main.py
+++ cornell_movie_dialogs_corpus/main.py
@@ -177,7 +177,7 @@ if __name__ == '__main__':
         glove_vectors = None
 
     print("Generating vocabulary and sentence pairs...")
-    vocabulary, sent_pairs = utils.prepare_training_data(dataset[:1000])
+    vocabulary, sent_pairs = utils.prepare_training_data(dataset)
     dev = torch.device(config.device if torch.cuda.is_available() else "cpu")
 
     print("Performing train test split...")
